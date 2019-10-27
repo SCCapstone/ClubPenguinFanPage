a812af4f73af4982a7b23a0686c04b8484a0b907
EC2 Default User <ec2-user@ip-172-31-19-201.us-east-2.compute.internal>
2019-10-27 20:51:29 +0000

    mergeconflict

--- textanalysis/login/views.py
+++ textanalysis/login/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, you've reached the textanalysis login page")
+    return HttpResponse("Hello, we are staging a merge conflict, it's pretty cool...")
