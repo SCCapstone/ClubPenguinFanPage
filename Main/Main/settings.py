cbbd51d9c8504021edbd1959a25b25213fd619fd
Ainsley McWaters <mcwatera@gmail.com>
2019-11-22 18:01:51 -0500

    login form attempted fix

--- Main/Main/settings.py
+++ Main/Main/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth', #lets us use authentifications stuff
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
