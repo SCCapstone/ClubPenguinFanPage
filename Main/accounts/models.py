24b88f1f9fe8434c51642406acde2e2bb98cf913
Ubuntu <ubuntu@ip-172-31-29-228.us-east-2.compute.internal>
2019-11-26 23:25:03 +0000

    Made Models for Project and Document

--- Main/accounts/models.py
+++ Main/accounts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField(default="Title")
+    
+    def __str__(self):
+            return Project.title
+    
+class Document(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    text = models.TextField()
+    
+    def __str__(self):
+            return Document.text
