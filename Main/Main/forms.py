8503fbab5c98fd3fe4412697e84c57c4a62c8663
Suzanne Prentice <PrenticeS16@ashleyhall.org>
2019-12-03 09:41:54 -0500

    styling text boxes

--- Main/Main/forms.py
+++ Main/Main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class InputTextForm(forms.Form):
-    analysis_text = forms.CharField(widget= forms.Textarea, label="Input Text", max_length=1000)
-    stop_words = forms.CharField(widget= forms.Textarea, label="Stop Words", max_length=200)
+    analysis_text = forms.CharField(widget=forms.Textarea(attrs={'style': "width:100%;"}), label="Input Text", max_length=1000)
+    stop_words = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 40, 'style': "width:100%;"}), label="Stop Words", max_length=200)
