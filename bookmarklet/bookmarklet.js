fc655f7815410b4af9a6780c187fece5be850886
sem15 <43047472+sem15@users.noreply.github.com>
2020-04-24 13:59:20 -0400

    Update bookmarklet.js

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -6,9 +6,7 @@ if (window.getSelection)
 (function() {
   var text = selection();
   console.log("Selected Text:" + text);
-  console.log("Opening new tab!");
-  var newP = window.open('http://textpenguin.herokuapp.com/guesthome/', '_blank').focus();
-  console.log("New tab opened!");
-  //Doesn't actually focus on the new tab and therefore says it can't update value of null for "firstdocinput"
-  document.querySelector("firstdocinput").value = text;
+  window.location.href = "http://textpenguin.herokuapp.com/guesthome/";
+  document.getElementById("firstdocinput").value = text;
 })();
+
