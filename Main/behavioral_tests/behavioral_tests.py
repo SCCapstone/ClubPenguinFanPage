1d978a6d4a931d58b6833d43d41bb256b30af04b
sem15 <43047472+sem15@users.noreply.github.com>
2020-04-26 21:51:58 -0400

    Behavioral tests for create and delete projects

--- Main/behavioral_tests/behavioral_tests.py
+++ Main/behavioral_tests/behavioral_tests.py
@@ -197,15 +197,79 @@ def pos_test_no_text():
 		print(algo + " detected an error given no text.")
 		return 0
 
-# create project tests?
+#Tests if the user can create a project
+def create_project_test():
+	num = 0
+	driver = login_setup()
+	navlinks = driver.find_elements_by_class_name('nav-item')
+	navlinks[1].click()
+	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, \
+	"projectstuff")))
+	time.sleep(1)
+	driver.find_element_by_class_name("button").click()
+	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, \
+	"firstdocinput")))
+	time.sleep(1)
+	title = driver.find_element_by_id("titleinput")
+	title.send_keys("projecttest")
+	body = driver.find_element_by_id("firstdocinput")
+	body.send_keys("This is example input.")
+	driver.find_element_by_id("submitbuttoninput").click()
+	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, \
+	"flex-column")))
+	time.sleep(1)
+	projects = driver.find_elements_by_class_name("projectstuff")
+	i = 0
+	for n in projects:
+		proj = projects[i].find_element_by_class_name("individualproject")
+		title = proj.find_element_by_tag_name("h4")
+		if title.text == "projecttest":
+			num = 1
+			print("Project successfully created.")
+			return num
+		i = i + 1
+
+	print("Project failed to be created.")
+	return num
+
+
+#Tests to see if the user can delete a project
+#NOTE: Run after create_project_test() or it will automatically fail!
+#NOTE: This will delete all projects named "projecttest"!
+def delete_project_test():
+	num = 0
+	driver = login_setup()
+	navlinks = driver.find_elements_by_class_name('nav-item')
+	navlinks[1].click()
+	WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, \
+	"projectstuff")))
+	time.sleep(1)
+	projects = driver.find_elements_by_class_name("projectstuff")
+	i = 0
+	for n in projects:
+		proj = projects[i].find_element_by_class_name("individualproject")
+		title = proj.find_element_by_tag_name("h4")
+		if title.text == "projecttest":
+			projects[i].find_element_by_class_name("deleteprojbutton").click()
+			num = 1
+			alert_obj = driver.switch_to.alert
+			alert_obj.accept()
+			print("Project successfully deleted.")
+		i = i + 1
+
+	if num == 0:
+		print("Project failed to be deleted.")
+
+	return num
 
 #Runs all tests and tracks how many pass or fail
 def test_suite():
 	num_passed = correct_login() + incorrect_login() + tfidf_test_text() + \
 	tfidf_test_no_text() + lda_test_text() + lda_test_no_text() + \
-	pos_test_text() + pos_test_no_text()
+	pos_test_text() + pos_test_no_text() + create_project_test() + \
+	delete_project_test()
 	print("\n ... \n")
-	print("Passed Tests: " , num_passed , "out of", 8)
+	print("Passed Tests: " , num_passed , "out of", 10)
 
 
 test_suite()
